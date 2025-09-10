b4669b4807896d1616ea331491ec314bc646dcd1
nalapapaya <madeline_lim_sy@hotmail.com>
2025-09-11 00:58:00 +0800

    added skip button

--- frontend/src/components/CafeGame.jsx
+++ frontend/src/components/CafeGame.jsx
@@ -8,6 +8,17 @@ const CafeGame = () => {
 
   const handleFeed = () => {
     setIsFed(true);
+    setHeartCount((prev) => prev + 10);
+    setCoinCount((prev) => prev + 50);
+
+    setTimeout(() => { //to reset after 2s
+    setIsFed(false);
+  }, 2000);
+  };
+
+  const handleSkip = () => {
+    setIsFed(false);
+    setHeartCount((prev) => prev - 2);
   };
 
   return (
@@ -22,20 +33,39 @@ const CafeGame = () => {
         </div>
         <div className={styles.speechCtn}>
           {!isFed ? (
-            <img
-              src="./src/assets/foods/Menu/pupcake.png"
-              alt="Food Item"
-              className={styles.speechItem}
-            />
+            <>
+              <img
+                src="./src/assets/foods/Menu/pupcake.png"
+                alt="Food Item"
+                className={styles.speechItem}
+              />
+              <img
+                src="./src/assets/gamePlay/pandaBowlEmpty.png"
+                alt="Empty Bowl"
+                className={styles.bowl}
+              />
+            </>
           ) : (
-            <img
-              src="./src/assets/gamePlay/pinkHeart.png"
-              alt="Food Item"
-              className={styles.speechHeart}
-            />
+            <>
+              <img
+                src="./src/assets/gamePlay/pinkHeart.png"
+                alt="Food Item"
+                className={styles.speechHeart}
+              />
+              <img
+                src="./src/assets/gamePlay/pandaBowlFull.png"
+                alt="Filled Bowl"
+                className={styles.bowl}
+              />
+            </>
           )}
         </div>
-        <button onClick={handleFeed} className={styles.feedBtn}>feed</button>
+        <button onClick={handleFeed} className={styles.feedBtn}>
+          feed
+        </button>
+        <button onClick={handleSkip} className={styles.skipBtn}>
+          <span className={styles.skipText}>Skip</span>
+        </button>
       </div>
     </div>
   );
